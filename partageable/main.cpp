e156aa94548bfd07b74badb0a9f6c151e95be672
Jordan <jordankri@hotmail.com>
2017-01-20 15:51:32 +0100

    Devoir Projet Composant

---
+++ partageable/main.cpp
@@ -0,0 +1,24 @@
+#define WINDOWS_LEAN_AND_MEAN
+#include <windows.h>
+#include <iostream>
+#include<conio.h>
+#include "composant1.h"
+#include "composant2.h"
+
+int main(int argc, char * argv)
+{
+	int data1=3;
+	int data2=5;
+
+	int valeur1;
+	int valeur2;
+
+	valeur1=composant1(data1,data2);
+
+	valeur2=composant2(data1,data2);
+
+	std::cout << getComposant1Version() << std::endl;
+	std::cout << "valeur 1 :" << valeur1 << " valeur 2 :" << valeur2 << std::endl;
+	getch();
+	Sleep(1000);
+}
